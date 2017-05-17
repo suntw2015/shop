d95a6bc7ec82ea93f03e847b342fc9a673d6cefe
tianwensun <631884352@qq.com>
2017-05-17 19:05:35 +0800

    add product

--- manage/controllers/Product.php
+++ manage/controllers/Product.php
@@ -2,13 +2,90 @@
 
 class Product extends APP_Controller{
 
+	protected $statusText = array(
+        '1' => '正常',
+        '2' => '下线'
+    );
+
 	public function __construct(){
 		parent::__construct();
+		$this->load->model(array('Product_model'));
 	}
 
 	public function index(){
-		$this->render('product/index.html',array(
-			'title'	=> '产品列表'
-		));
-	}
+        $productList = $this->Product_model->get();
+
+        foreach($productList as $key=>&$value){
+            $value['status_text'] = isset($this->statusText[$value['status']]) ? $this->statusText[$value['status']] : '';
+        }
+
+        $this->render('product/index.html',array(
+            'title'         => '产品管理',
+            'productList'  => $productList
+        ));
+    }
+
+    public function create(){
+        $this->render('product/create.html',array(
+            'title'         => '新建产品',
+            'statusList'    => $this->statusText
+        ));
+    }
+
+    public function do_create(){
+        $data = $this->input->post();
+        if(empty($data) || !is_array($data)){
+            $this->return_fail('创建失败，产品信息为空');
+        }
+
+        $data['create_by'] = $this->userInfo['id'];
+        $res = $this->Product_model->create($data);
+        if(!is_int($res)){
+            $this->return_fail($res);
+        }
+
+        $this->return_success('创建成功');
+    }
+
+    public function update(){
+        $data = $this->input->get();
+
+        $data['id'] = (int)$data['id'];
+        $productInfo = $this->Product_model->getProductById($data['id']);
+        $this->render('product/update.html',array(
+            'productInfo'   => $productInfo,
+            'statusList'    => $this->statusText
+        ));
+    }
+
+    public function do_update(){
+        $data = $this->input->post();
+        if(empty($data) || !is_array($data)){
+            $this->return_fail('更新失败，分类信息为空');
+        }
+
+        $data['update_by'] = $this->userInfo['id'];
+        $res = $this->Product_model->update($data);
+        if(!is_int($res)){
+            $this->return_fail($res);
+        }
+
+        $this->return_success('更新成功');
+    }
+
+    public function delete(){
+        $data = $this->input->post();
+        if(!isset($data['id'])){
+            $this->return_fail('删除失败');
+        }
+
+        $data['id'] = (int)$data['id'];
+        $res = $this->Product_model->deleteProductById($data['id']);
+
+        if(!is_int($res) || $res < 1){
+            $this->return_fail($res);
+        }
+
+        $this->return_success('删除成功');
+    }
 }
--- manage/models/Product_model.php
+++ manage/models/Product_model.php
@@ -8,46 +8,80 @@ class Product_model extends APP_Model{
         $this->load->database();
     }
 
-    public function get($data){
-
+    public function get(){
+        $query = $this->db->get('product');
+        $res = $query->result_array();
+        return $res;
     }
 
-    public function multi_get($data){
-        $checkedParams = array(
-            'id'        => $this->check_param($data,'id','string'),
-            'title'     => $this->check_param($data,'title','string'),
-            'shop_id'   => $this->check_param($data,'shop_id','int'),
-            'status'    => $this->check_param($data,'status','int')
+    public function create($data){
+        $checkField = array(
+            'name'  => '名称',
+            'desc'  => '描述',
+            'cover_img' => '缩略图',
+            'stock'     => '库存',
+            'price'     => '价格',
+            'status'    => '状态'
         );
-
-        if(!empty($checkedParams['id'])){
-            $id = explode(',',$checkedParams['id']);
-            if(count($id) == 1){
-                $this->db->where('id',$id[0]);
-            }else{
-                $this->db->where_in('id',$id);
+        
+        foreach($checkField as $key=>$value){
+            if(empty($data[$key])){
+                return $value."不能为空";
             }
         }
 
-        if(!empty($checkedParams['title'])){
-            $this->db->like('title',$checkedParams['title']);
-        }
+        $info = array(
+            'name'  => $data['name'],
+            'desc'  => $data['desc'],
+            'cover_img'     => $data['cover_img'],
+            'stock'         => $data['stock'],
+            'price'         => $data['price'],
+            'status'        => $data['status'],
+            'create_by'     => $data['create_by'],
+            'create_time'   => date('Y-m-d H:i:s',time())
+        );
 
-        if(!empty($checkedParams['shop_id'])){
-            $this->db->where('shop_id',$checkedParams['shop_id']);
-        }
-
-        if(!empty($checkedParams['status'])){
-            $this->db->where('status',$checkedParams['status']);
-        }
-
-        $query = $this->db->get('product');
-        $result = $query->result_array();
+        $this->db->insert('product',$info);
+        $result = $this->db->insert_id();
 
         return $result;
     }
 
-    public function create($data){
-        
+    public function update($data){
+        if(empty($data['pid'])){
+            return '产品ID不能为空';
+        }
+
+        $data['pid'] = (int)$data['pid'];
+
+        if(!empty($data['status']) && !in_array($data['status'],array(1,2))){
+            return '状态不正确';
+        }
+
+        $categoryInfo = $this->getCategoryByid($data['cid']);
+        if(empty($categoryInfo)){
+            return '分类信息不存在';
+        }
+
+        $updateField = array('name','desc','cover_img','stock','price','status');
+
+        foreach($updateField as $value){
+            if(!empty($data[$value]) && $data[$value] != $categoryInfo[$value]){
+                $this->db->set($value,$data[$value]);
+            }
+        }
+
+        $this->db->set('update_by',$data['update_by']);
+        $this->db->set('update_time',date('Y-m-d H:i:s',time()));
+
+        $this->db->where('id',$data['pid']);
+        $this->db->update('product');
+        return $this->db->affected_rows();
+    }
+
+    public function getProductById($id){
+        $query = $this->db->get_where('product',array('id'=>$id));
+        $res = $query->row_array();
+        return $res;
     }
 }
